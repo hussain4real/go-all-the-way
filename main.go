369ef87a3409e9dc549431cb6f0d02d11759bfcb
Hussain Aminu <hussain_aminu@yahoo.com>
2023-03-23 23:20:00 +0300

    (): WIP

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
 	"net/http"
@@ -8,9 +9,19 @@ import (
 )
 
 var recipes []Recipe
+var chefs []Chef
 
 func init() {
 	recipes = make([]Recipe, 0)
+	chefs = make([]Chef, 0)
+}
+
+type Chef struct {
+	Id                string    `json:"id"`
+	Name              string    `json:"name"`
+	Country           string    `json:"country"`
+	YearsOfExperience int       `json:"yearsOfExperience"`
+	Recipes           []*Recipe `json:"recipes"`
 }
 
 type Recipe struct {
@@ -20,6 +31,7 @@ type Recipe struct {
 	Ingredients  []string  `json:"ingredients"`
 	Instructions []string  `json:"instructions"`
 	PublishedAt  time.Time `json:"publishedAt"`
+	ChefId       string    `json:"chefId"`
 }
 
 func DeleteRecipeHandler(c *gin.Context) {
@@ -73,16 +85,58 @@ func UpdateRecipeHandler(c *gin.Context) {
 		return
 	}
 
+	// Find the chef for the recipe using the chef ID in the request body
+	chefID := recipe.ChefId
+	var chef *Chef
+	for i := range chefs {
+		if chefs[i].Id == chefID {
+			chef = &chefs[i]
+			break
+		}
+	}
+	if chef == nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("Chef with ID %s not found", chefID),
+		})
+		return
+	}
+
+	// Remove the recipe from the old chef's list of recipes
+	oldChefID := recipes[index].ChefId
+	oldChef := getChefByID(oldChefID)
+	if oldChef != nil {
+		oldRecipes := oldChef.Recipes
+		for i, r := range oldRecipes {
+			if r.Id == id {
+				oldChef.Recipes = append(oldRecipes[:i], oldRecipes[i+1:]...)
+				break
+			}
+		}
+	}
+
+	// Add the recipe to the new chef's list of recipes
+	chef.Recipes = append(chef.Recipes, &recipe)
+
 	recipe.Id = id
 	recipes[index] = recipe
 
 	c.JSON(http.StatusOK, recipe)
 }
 
+func getChefByID(id string) *Chef {
+	for i := range chefs {
+		if chefs[i].Id == id {
+			return &chefs[i]
+		}
+	}
+	return nil
+}
+
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+// function to create new recipe
 func NewRecipeHandler(c *gin.Context) {
 	var recipe Recipe
 
@@ -95,7 +149,29 @@ func NewRecipeHandler(c *gin.Context) {
 
 	recipe.Id = xid.New().String()
 	recipe.PublishedAt = time.Now()
+
+	// Find the chef for the recipe using the chef ID in the request body
+	chefID := recipe.ChefId
+	var chef *Chef
+	for i := range chefs {
+		if chefs[i].Id == chefID {
+			chef = &chefs[i]
+			break
+		}
+	}
+	if chef == nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("Chef with ID %s not found", chefID),
+		})
+		return
+	}
+
+	// Add the recipe to the chef's list of recipes
+	chef.Recipes = append(chef.Recipes, &recipe)
+
+	// Append the recipe to the global list of recipes
 	recipes = append(recipes, recipe)
+
 	c.JSON(http.StatusOK, recipe)
 }
 
@@ -105,5 +181,8 @@ func main() {
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("recipes/:recipe-id", UpdateRecipeHandler)
 	router.DELETE("recipes/:recipe-id", DeleteRecipeHandler)
-	router.Run()
+	err := router.Run()
+	if err != nil {
+		return
+	}
 }
